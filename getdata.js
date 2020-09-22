05df3782bc6fc154f84f71dc4873098f632033c9
Violeta Rodriguez <vl.rodriguez10@uniandes.edu.co>
2020-09-22 18:46:59 -0500

    Avance para primera entrega

---
+++ getdata.js
@@ -0,0 +1,4 @@
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let url =
+    "https://gist.githubusercontent.com/josejbocanegra/be0461060d1c2d899740b8247089ba22/raw/916d2141e32e04031bda79c8886e8e4df0ae7f24/productos.jsonhttps://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
