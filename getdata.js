1cbc4bd782cf4a606e15bea7f5971d325526f064
Violeta Rodriguez <vl.rodriguez10@uniandes.edu.co>
2020-09-23 14:32:16 -0500

    Casi listo lo funcional.

--- getdata.js
+++ getdata.js
@@ -2,9 +2,24 @@
 let url =
     "https://gist.githubusercontent.com/josejbocanegra/9a28c356416badb8f9173daf36d1460b/raw/5ea84b9d43ff494fcbf5c5186544a18b42812f09/restaurant.json";
 
+
+/**
+ * Configuraciones iniciales
+ */
 var numCarItems = 0;
-var carItems=[];
+var carItems = [];
 updateNumCarItems(numCarItems);
+car = document.getElementById("btn-car");
+car.addEventListener("click", () => viewCarItems())
+ordDetail = document.getElementById("carDetail");
+ordDetail.style.visibility = "hidden";
+
+btnCfO = document.getElementById("btn-cofirm");
+btnCfO.addEventListener("click", confirmOrder());
+
+btnCnO=document.getElementById("btn-cancel-order");
+btnCnO.addEventListener("click", cancerlOrder());
+
 
 
 let data;
@@ -56,10 +71,10 @@ function createCards(list) {
         buttom.innerHTML = "Add to car";
         buttom.addEventListener("click", () => {
             carItems.push(product);
-            numCarItems ++;
+            numCarItems++;
             updateNumCarItems(numCarItems)
             console.log(carItems);
-        } );
+        });
         body.appendChild(buttom);
 
 
@@ -74,17 +89,128 @@ function updateNumCarItems(number) {
     if (number === 0) {
         pItems.innerHTML = "";
     }
-    else if (number==1)
-    {
-        pItems.innerHTML = number +" item";
+    else if (number == 1) {
+        pItems.innerHTML = number + " item";
     }
-    else{
-        pItems.innerHTML = number +" items";
+    else {
+        pItems.innerHTML = number + " items";
 
     }
 }
 
-function viewCarItems()
-{
-    
+function viewCarItems() {
+    totalPrice = 0;
+    contador = 1;
+    names = [];
+    ordDetail = document.getElementById("carDetail");
+    ordDetail.style.visibility = "visible";
+
+    table = document.getElementById("itemsTable");
+
+    thead = document.createElement("thead");
+
+    row = document.createElement("tr");
+    item = document.createElement("th");
+    item.setAttribute("scope", "col");
+    textCell = document.createTextNode("item");
+    item.appendChild(textCell);
+    row.appendChild(item);
+
+    qty = document.createElement("th");
+    qty.setAttribute("scope", "col");
+    textCell = document.createTextNode("Qty");
+    qty.appendChild(textCell);
+    row.appendChild(qty);
+
+    des = document.createElement("th");
+    des.setAttribute("scope", "col");
+    textCell = document.createTextNode("Description");
+    des.appendChild(textCell);
+    row.appendChild(des);
+
+    up = document.createElement("th");
+    up.setAttribute("scope", "col");
+    textCell = document.createTextNode("Unitary Price ($)");
+    up.appendChild(textCell);
+    row.appendChild(up);
+
+    amt = document.createElement("th");
+    amt.setAttribute("scope", "col");
+    textCell = document.createTextNode("Amount ($)");
+    amt.appendChild(textCell);
+    row.appendChild(amt);
+
+    thead.appendChild(row);
+    table.appendChild(thead);
+
+
+    body = document.createElement("tbody");
+    carItems.forEach(product => {
+
+        /* if (names.includes(product["name"])) {
+  
+              amt= document.getElementById(product["name"]);
+              console.log(product["name"]);
+              textCell = document.createTextNode(parseFloat(amt.value) + product["price"]);
+              qty.appendChild(textCell);
+              
+  
+          }
+          else {*/
+        row = document.createElement("tr");
+
+        item = document.createElement("td");
+        item.setAttribute("scope", "col");
+        textCell = document.createTextNode(contador);
+        item.appendChild(textCell);
+        row.appendChild(item);
+
+        qty = document.createElement("td");
+        qty.setAttribute("scope", "col");
+        textCell = document.createTextNode(1);
+        qty.appendChild(textCell);
+        row.appendChild(qty);
+
+        des = document.createElement("td");
+        des.setAttribute("scope", "col");
+        textCell = document.createTextNode(product["name"]);
+        names.push(product["name"]);
+        des.appendChild(textCell);
+        row.appendChild(des);
+
+        up = document.createElement("td");
+        up.setAttribute("scope", "col");
+        textCell = document.createTextNode(product["price"]);
+        up.appendChild(textCell);
+        row.appendChild(up);
+
+        amt = document.createElement("td");
+        amt.setAttribute("scope", "col");
+        amt.setAttribute("id", product["name"]);
+        textCell = document.createTextNode(product["price"]);
+        amt.appendChild(textCell);
+        row.appendChild(amt);
+
+        body.appendChild(row);
+
+        contador++;
+        totalPrice += product["price"];
+        //  }
+    });
+
+
+    table.appendChild(body);
+
+    totalrow = document.createElement("row")
+    total = document.createElement("r")
 }
+
+function confirmOrder() {
+    console.log(carItems);
+}
+
+function cancerlOrder()
+ {
+    numCarItems=0;
+    carItems=[];   
+ }
